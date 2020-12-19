cab19979a3b120960117436837f661235e1f2e52
jazhawang <jazhawang@gmail.com>
2020-12-19 13:38:10 -0500

    add some eigen types and include plus src folders

---
+++ include/fluidsim.h
@@ -0,0 +1,6 @@
+
+// MAIN fluid variables
+#define DENSITY 0.0
+#define VISCOSITY 0.0
+
+#define NUM_PARTICLES 8
--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 //
-// Created by clemence on 2020-12-19.
+// Created by clemence on 2020-12-19. AND JACK
 //
 #include <GLFW/glfw3.h>
+#include <fluidsim.h>
+
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
 
 int main(int argc, char **argv) {
+
+    /*  Generalized coordinates is a 2n vector of particle positions:
+        [x1, y1, x2, y2, ...] , where n is the number of fluid particles defined
+        in simulation. */  
+    Eigen::VectorXf q = Eigen::VectorXf::Zero(2*NUM_PARTICLES);
+
+    /*  Generalized velocity of fluid particles */
+    Eigen::VectorXf qdot = Eigen::VectorXf::Zero(2*NUM_PARTICLES);
+
     glfwInit();
 
     // Create window
@@ -15,6 +28,8 @@ int main(int argc, char **argv) {
     glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
     GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", nullptr, nullptr);
     glfwMakeContextCurrent(window);
+
+    // initialize main sim variables here
 
     while(!glfwWindowShouldClose(window))
     {
