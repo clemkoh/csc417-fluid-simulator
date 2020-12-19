e5344a8938f9696481619f8e0ea03e577a91a705
clemencetbk <kohclemence@gmail.com>
2020-12-19 02:57:23 -0500

    Add CMakeLists and code for empty window

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by clemence on 2020-12-19.
+//
+#include <GLFW/glfw3.h>
+
+int main(int argc, char **argv) {
+    glfwInit();
+
+    // Create window
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+    GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+}
